383d85ee74177a2fe00eb5e2f455997a4eea6cc5
Johnnygames <johnegames@gmail.com>
2015-02-13 16:44:22 -0800

    Very very basic implmentation of a SVG 'enemy' or circle

--- watchout.js
+++ watchout.js
@@ -1,2 +1,3 @@
 // start slingin' some d3 here.
-
+var gameBoard = d3.select("body").append("svg").attr("width",800).attr("height",400);
+var enemies = gameBoard.append("circle").attr("cx",20).attr("cy",20).attr("r",20).attr("fill","blue");
